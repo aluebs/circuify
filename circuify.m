293ed7503183b02278add732e489312f1f6ad484
Alejandro Luebs <alejandroluebs@gmail.com>
2016-07-24 16:44:28 -0700

    Fix normalization and restrict correlation

--- circuify.m
+++ circuify.m
@@ -1,16 +1,20 @@
-function [rcv, rch, rrv, rrh] = circuify(img)
-  mindetail = 0.0005;
-  surfacewidth = 0.05;
+function [x, rcv, rch, rrv, rrh] = circuify(img)
+  mindetail = 0.001;
+  surfacewidth = 0.07;
   N = surfacewidth / mindetail;
+
   x = imgcondition(imgread(img), N);
+
   kc = normalizekernel([1, 0, 1, 1, 1, 0, 1; ...
                         0, 0, 1, 1, 1, 0, 0; ...
                         1, 0, 1, 1, 1, 0, 1]);
+  rcv = crosscorr(x, kc);
+  rch = crosscorr(x, kc.');
+
+
   kr = normalizekernel([1, 0, 1]);
-  rcv = xcorr2(x, kc);
-  rch = xcorr2(x, kc.');
-  rrv = xcorr2(x, kr);
-  rrh = xcorr2(x, kr.');
+  rrv = crosscorr(x, kr);
+  rrh = crosscorr(x, kr.');
 end
 
 function [x] = imgread(img)
@@ -23,7 +27,7 @@ end
 function [y] = imgcondition(x, N)
   y = rgb2gray(x);
   y = imcomplement(y);
-  y = y / max(y(:));
+  y = mat2gray(y);
   y = crop2square(y);
   y = imresize(y, [N, N]);
 end
@@ -38,4 +42,12 @@ function [y] = normalizekernel(x)
   m = mean(x(:));
   y = x - m;
   y = y / sum(sum(x .* y));
+end
+
+function [r] = crosscorr(x, k)
+  [N, M] = size(k);
+  r = xcorr2(x, k);
+  r = r(N:end, M:end);
+  r((end - N + 2):end, :) = 0;
+  r(:, (end - M + 2):end) = 0;
 end
