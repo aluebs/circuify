a78d4eba1e38a8f93548115a8347877401c84ad5
Alejandro Luebs <alejandroluebs@gmail.com>
2016-09-18 18:41:03 -0700

    Adding 2-sided kernels

--- circuify.m
+++ circuify.m
@@ -4,7 +4,8 @@ function [x, y] = circuify(img)
   N = round(surfacewidth / mindetail);
   x = imgread(img, N);
   y = zeros(N, N);
-  y = addfoursided(x, y);
+  %y = addfoursided(x, y);
+  y = addtwosided(x, y);
 end
 
 function [x] = imgread(img, N)
@@ -36,7 +37,7 @@ function [y] = addfoursided(x, y)
   rmin = rmax / 2;
   while rmax > rmin
     y = addkernel(y, k{m}, n1, n2);
-    r = updatecorrs(r, k, m, n1, n2);
+    r = updatecorrs(r, k, k{m}, n1, n2);
     [rmax, m, n1, n2] = maxcorr(r);
   end
 end
@@ -61,12 +62,66 @@ function [k] = foursidedkernel(M)
   k = normalizekernel(k);
 end
 
+function [y] = addtwosided(x, y)
+  k = twosidedkernels();
+  r = crosscorrs(x, k);
+  [rmax, m, n1, n2] = maxcorr(r);
+  rmin = rmax / 2;
+  while rmax > rmin
+    [y, kf, n1, n2] = addtwosidedkernel(y, r{m}, k{m}, m, n1, n2);
+    r = updatecorrs(r, k, kf, n1, n2);
+    [rmax, m, n1, n2] = maxcorr(r);
+  end
+end
+
+function [k] = twosidedkernels(N)
+  k = cell(2, 1);
+  k{1} = twosidedkernel(2);
+  k{2} = k{1}.';
+end
+
 function [k] = twosidedkernel(M)
   k = zeros(2 * M - 1, 7);
   k(1:2:end, [1, 7]) = 1;
   k(:, [3, 5]) = 1;
   k([1, end], 4) = 1;
   k = normalizekernel(k);
+end
+
+function [y, kf, n1, n2] = addtwosidedkernel(y, r, k, m, n1, n2)
+  [N1, N2] = size(r);
+  switch m
+    case 1
+      s1 = 2;
+      s2 = 0;
+    case 2
+      s1 = 0;
+      s2 = 2;
+  end
+  if ((n1 + s1) > N1) || ...
+     ((n2 + s2) > N2) || ...
+     ((n1 > s1) && (n2 > s2) && (r(n1 - s1, n2 - s2) > r(n1 + s1, n2 + s2)))
+    s1 = -s1;
+    s2 = -s2;
+  end
+  rmin = r(n1, n2) / 2;
+  ns = 0;
+  rf = r(n1 + (ns + 1) * s1, n2 + (ns + 1) * s2);
+  while rf > rmin
+    ns = ns + 1;
+    rf = r(n1 + (ns + 1) * s1, n2 + (ns + 1) * s2);
+  end
+  switch m
+    case 1
+      kf = twosidedkernel(ns + 2);
+    case 2
+      kf = twosidedkernel(ns + 2).';
+  end
+  if s1 + s2 < 0
+    n1 = n1 + ns * s1;
+    n2 = n2 + ns * s2;
+  end
+  y = addkernel(y, kf, n1, n2);
 end
 
 function [k] = resistencekernel()
@@ -115,10 +170,10 @@ function [y] = addkernel(y, k, n1, n2)
   y(n1:(n1 + N1 - 1), n2:(n2 + N2 - 1)) = k > 0;
 end
 
-function [r] = updatecorrs(r, k, mk, n1, n2)
-  for mr = 1:length(r)
-    [N1r, N2r] = size(k{mr});
-    [N1k, N2k] = size(k{mk});
-    r{mr}(max(0, n1 - N1r):(n1 + N1k), max(0, n2 - N2r):(n2 + N2k)) = 0;
+function [r] = updatecorrs(r, k, kf, n1, n2)
+  [N1k, N2k] = size(kf);
+  for m = 1:length(r)
+    [N1r, N2r] = size(k{m});
+    r{m}(max(0, n1 - N1r):(n1 + N1k), max(0, n2 - N2r):(n2 + N2k)) = 0;
   end
 end
